ce17e0df336d25d3b7e706cb2b252b90a0639b42
jarteaga-applaudo <jarteaga@applaudostudios.com>
2019-08-19 19:06:34 -0600

    :construction: Response Builder Refactor

---
+++ DiagnostikaNexusCore/BLL/Hl7ResponseBuilder/ResponseBuilder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DiagnostikaNexusCore.DOT;
+using DiagnostikaNexusCore.DOT.Hl7Response;
+using DiagnostikaNexusCore.BLL.Util;
+
+namespace DiagnostikaNexusCore.BLL.Hl7ResponseBuilder
+{
+    public class ResponseBuilder
+    {
+        
+
+        public Response buildResponse() {
+            Response response = new Response();
+
+
+            return response;
+        }
+
+        private Response buildHeader() {
+            MSHHeader mSHHeader = new MSHHeader();
+            mSHHeader.Msh_1_fielSeparador = CoreConfiguration.FieldSeparator;
+            mSHHeader.Msh_2_EncodeingCaracters = CoreConfiguration.EncodeingCaracters;
+            mSHHeader.Msh_3_sendingApplication = CoreConfiguration.ApplicationName;
+           
+
+
+            return mSHHeader;
+        }
+    }
+}
